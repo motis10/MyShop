8dbcaf0ed4cc0b388199d988c978d2cdd269ffdc
zahi101 <zahi101@gmail.com>
2021-04-13 19:53:31 +0300

    logout bug fix(reset orders and user state)

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -5,6 +5,11 @@ import {
     ORDER_DETAILS_REQUEST,
     ORDER_DETAILS_SUCCESS,
     ORDER_DETAILS_FAIL,
+    ORDER_LIST_MY_REQUEST,
+  ORDER_LIST_MY_SUCCESS,
+  ORDER_LIST_MY_FAIL,
+  ORDER_LIST_MY_RESET,
+
   } from '../constants/orderConstants'
   
   export const orderCreateReducer = (state = {}, action) => {
@@ -30,7 +35,7 @@ import {
   }
 
   export const orderDetailsReducer = (
-    state = { orderItems: [], shippingAddress: {} },
+    state = { loading: true, orderItems: [], shippingAddress: {} },
     action
   ) => {
     switch (action.type) {
@@ -49,6 +54,29 @@ import {
           loading: false,
           error: action.payload,
         }
+      default:
+        return state
+    }
+  }
+
+  export const orderListMyReducer = (state = { orders: [] }, action) => {
+    switch (action.type) {
+      case ORDER_LIST_MY_REQUEST:
+        return {
+          loading: true,
+        }
+      case ORDER_LIST_MY_SUCCESS:
+        return {
+          loading: false,
+          orders: action.payload,
+        }
+      case ORDER_LIST_MY_FAIL:
+        return {
+          loading: false,
+          error: action.payload,
+        }
+        case ORDER_LIST_MY_RESET:
+          return { orders: [] }
       default:
         return state
     }
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -8,6 +8,7 @@ import {
     USER_REGISTER_SUCCESS,  
     USER_DETAILS_FAIL,
     USER_DETAILS_REQUEST,
+    USER_DETAILS_RESET,
     USER_DETAILS_SUCCESS,
     USER_UPDATE_PROFILE_FAIL,
     USER_UPDATE_PROFILE_REQUEST,
@@ -51,6 +52,8 @@ import {
         return { loading: false, user: action.payload }
       case USER_DETAILS_FAIL:
         return { loading: false, error: action.payload }
+        case USER_DETAILS_RESET:
+          return { user: {} }
       default:
         return state
     }

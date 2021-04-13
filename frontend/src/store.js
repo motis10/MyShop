81a61ad888ce255638a70f4db8230e82bca8b9fc
zahi101 <zahi101@gmail.com>
2021-04-13 13:33:47 +0300

    place order continue+upload order to mongo

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,6 +12,8 @@ import {
   userDetailsReducer,
   userUpdateProfileReducer,
 } from './reducers/userReducers'
+import { orderCreateReducer } from './reducers/orderReducers'
+
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -21,6 +23,8 @@ const reducer = combineReducers({
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,  
+    orderCreate: orderCreateReducer,
+
   })
   
 const cartItemsFromStorage = localStorage.getItem('cartItems')

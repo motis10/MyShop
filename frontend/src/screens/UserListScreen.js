827b98e4120c18055549cd6649aac98afd370fe1
zahi101 <zahi101@gmail.com>
2021-04-14 11:09:10 +0300

    user delete add to userListScreen

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -4,7 +4,7 @@ import { Table, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { listUsers } from '../actions/userActions'
+import { listUsers, deleteUser } from '../actions/userActions'
 
 const UserListScreen = ({ history }) => {
     const dispatch = useDispatch()
@@ -14,6 +14,10 @@ const UserListScreen = ({ history }) => {
 
   const userLogin = useSelector((state) => state.userLogin)
   const { userInfo } = userLogin
+
+  const userDelete = useSelector((state) => state.userDelete)
+  const { success: successDelete } = userDelete
+
 
 
   useEffect(() => {
@@ -25,11 +29,12 @@ const UserListScreen = ({ history }) => {
   } else {
     history.push('/login')
   }
-}, [dispatch, history])
+}, [dispatch, history, successDelete])
 
   const deleteHandler = (id) => {
-    console.log('delete')
-  }
+    if (window.confirm('Are you sure')) {
+      dispatch(deleteUser(id))
+    }  }
 
   return (
     <>

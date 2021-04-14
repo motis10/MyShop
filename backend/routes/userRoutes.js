607a4d3c14dcff44b72256859f66f7bc709d2490
zahi101 <zahi101@gmail.com>
2021-04-14 11:08:53 +0300

    user delete backend init

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,6 +6,8 @@ import {
     getUserProfile,
     updateUserProfile,
     getUsers,
+    deleteUser,
+
   } from '../controllers/userController.js'
   import { protect, admin } from '../middleware/authMiddleware.js'
 
@@ -15,5 +17,7 @@ router
   .route('/profile')
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile)
+  router.route('/:id').delete(protect, admin, deleteUser)
+
 
 export default router

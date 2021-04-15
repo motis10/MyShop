bd303a7c46a2e3b0a5503913a17edf2cdd50a09b
Moti Stein <moti.stein@gmail.com>
2021-04-15 21:25:02 +0300

    edit profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,12 +16,7 @@ import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
 import ProductListScreen from './screens/ProductListScreen'
-
-
-
-
-
-
+import ProductEditScreen from './screens/ProductEditScreen'
 
 const App = () => {
   return (
@@ -42,7 +37,7 @@ const App = () => {
           <Route path='/placeorder' component={PlaceOrderScreen} />
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
           <Route path='/admin/productlist' component={ProductListScreen} />
-
+          <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
 
 
         </Container>

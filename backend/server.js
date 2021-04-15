05c5a5678a03842e823eb514e234fbaefd8a73ac
Moti Stein <moti.stein@gmail.com>
2021-04-15 21:24:47 +0300

    fix for server

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import colors from 'colors'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import path from 'path'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 
 dotenv.config()
@@ -23,12 +25,16 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 
 app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.listen(
   PORT,

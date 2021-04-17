f4ee1a562bc580c40d00413b4bc613a6dc651f02
Moti Stein <moti.stein@gmail.com>
2021-04-17 22:11:56 +0300

    Add comments

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
+//First line render with store and all app is render
 ReactDOM.render(
   <Provider store={store}>
     <App />
